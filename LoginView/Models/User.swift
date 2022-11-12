47952654a59cc7d42921d99eabebd400730a8249
Nikita Tarasov <Owlcompany@icloud.com>
2022-11-12 17:08:17 +0300

    Create models User

---
+++ LoginView/Models/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  LoginView
+//
+//  Created by Mr Owlise on 12.11.2022.
+//
+
+struct User {
+    let userName = "Owlise"
+    let password = "1234"
+    let userData: Description
+}
+
+struct Description {
+    let age = 23
+    let name = "Nikita"
+    let surename = "Tarasov"
+    let aboutMe = "Печатаю разные штуки на 3д принтере интересуюсь разработкой и автоматизацией, в свободное время занимаеюсь 3д моделированием"
+}
